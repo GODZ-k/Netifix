a853b45d9a0a7c5a10d1b9ae489a10f812fd6b65
Tanmay khatri <131422684+GODZ-k@users.noreply.github.com>
2023-11-20 23:13:04 +0530

    pagination and filter

--- core/views.py
+++ core/views.py
@@ -4,18 +4,49 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
+    # pagination and search
     item=movie.objects.all()
+    if request.method == 'GET':
+      search=request.GET.get('search','')
+      if search != None:
+       item=movie.objects.filter(name__icontains=search)
     paginatordata=Paginator(item,1)
     page_no=request.GET.get("page")
     finaldata=paginatordata.get_page(page_no)
     lastpage=finaldata.paginator.num_pages
     total_page_no=[n+1 for n in range(lastpage)]
-    hot_thrill=hot_thrills.objects.all()
-    trend=trending.objects.all()
-    movie_tags=Tags.objects.all()
-    recomanded1=poster1.objects.all()
-    recomanded2=poster2.objects.all()
-    recomanded3=poster3.objects.all()
+    # hot thrills
+    hot_thrill_data_dict=hot_thirill_data()
+    # recommanded poster
+    poster_data=poster()
+    # category
+    category_data_dict=category_data()
+    # movie tags
+    movie_tags=tags()
+    # trending
+    movie_trend=trend()
+
+    data={
+        # pagination and search
+        "items": finaldata,
+        "lastpage":lastpage,
+        "total_page_no":total_page_no,
+        "search":search,
+       # hot thrills
+        **hot_thrill_data_dict,
+        # recommanded poster
+        **poster_data,
+        # movie trend
+        **movie_trend,
+        # movie tags
+        **movie_tags,
+        # category data
+        **category_data_dict,
+
+    }
+    return render(request, "index.html",data)
+
+def category_data():
     Scifi=movie.objects.filter(category__category="Scifi")
     Action=movie.objects.filter(category__category="Action")
     Advanture=movie.objects.filter(category__category="Advanture")
@@ -26,31 +57,51 @@ def home(request):
     Thriller=movie.objects.filter(category__category="Thriller")
     Mystery=movie.objects.filter(category__category="Mystery")
     Romance=movie.objects.filter(category__category="Romance")
-    data={
-        # pagination
-        "items": finaldata,
-        "lastpage":lastpage,
-        "total_page_no":total_page_no,
-        # hot thrills
+
+    return {
+        "Scifi": Scifi,
+        "Action": Action,
+        "Advanture": Advanture,
+        "Comedy": Comedy,
+        "Fantasy": Fantasy,
+        "History": History,
+        "Horror": Horror,
+        "Thriller": Thriller,
+        "Mystery": Mystery,
+        "Romance": Romance,
+    }
+
+def hot_thirill_data():
+    hot_thrill=hot_thrills.objects.all()
+    return {
         "slideshow": hot_thrill,
-        "trend":trend,
+    }
+
+def poster():
+    recomanded1=poster1.objects.all()
+    recomanded2=poster2.objects.all()
+    recomanded3=poster3.objects.all()
+
+    return {
         "recomanded1":recomanded1,
         "recomanded2":recomanded2,
         "recomanded3":recomanded3,
-        "Scifi":Scifi,
-        "Action":Action,
-        "Advanture":Advanture,
-        "Comedy":Comedy,
-        "Fantasy":Fantasy,
-        "History":History,
-        "Horror":Horror,
-        "Thriller":Thriller,
-        "Mystery":Mystery,
-        "Romance":Romance,
-        "movie_tags":movie_tags
-
     }
-    return render(request, "index.html",data)
+
+def tags():
+    movie_tags=Tags.objects.all()
+
+    return {
+        "movie_tags":movie_tags,
+    }
+
+
+def trend():
+    trend=trending.objects.all()
+
+    return {
+      "trend":trend,
+    }
 
 def dmca(request):
     return render(request,"dmca.html")
